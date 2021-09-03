80cc3c3c8bd45fc06ed88e0b587ecbe5070cd6c0
csatswo <68028099+csatswo@users.noreply.github.com>
2021-09-02 17:00:30 -0700

    Updates
    Replace autoattendants function with separate functions

---
+++ Functions/fn_aaresourceaccounts.ps1
@@ -0,0 +1,90 @@
+﻿Function AAResourceAccounts {
+param([Parameter(mandatory=$false)][String]$Name)
+$assignedAccounts = @()
+if ($Name) {
+    $resourceAccountAAs = @()
+    $resourceAccountAAs = @(Get-CsAutoAttendant | Where-Object {$_.Name -like "$Name"})
+    if ($resourceAccountAAs.Count -gt 1) {
+        foreach ($resourceAccountAA in $resourceAccountAAs) {
+            Write-Host "Auto Attendant $($resourceAccountAA.Name) found" -ForegroundColor Cyan
+            $resourceAccounts = $resourceAccountAA.ApplicationInstances
+            if ($resourceAccounts) {
+                foreach ($resourceAccount in $resourceAccounts) { 
+                    $appInstance = Get-CsOnlineApplicationInstance -Identity $resourceAccount
+                    $customProperties = @{
+                        AutoAttendant = $resourceAccountAA.Name
+                        DisplayName = $appInstance.DisplayName
+                        UserPrincipalName = $appInstance.UserPrincipalName
+                        LineUri = $appInstance.PhoneNumber
+                    }
+                    $assignedAccounts += New-Object -TypeName PSObject -Property $customProperties
+                }
+            } else {
+                Write-Host "Auto Attendant $($resourceAccountAA.Name) has no resource account assigned" -ForegroundColor Yellow
+            }
+        }
+    } elseif ($resourceAccountAAs.Count -eq 1) {
+        Write-Host "Auto Attendant $($resourceAccountAA.Name) found" -ForegroundColor Cyan
+        $resourceAccounts = $resourceAccountAA.ApplicationInstances
+        if ($resourceAccounts) {
+            foreach ($resourceAccount in $resourceAccounts) { 
+                $appInstance = Get-CsOnlineApplicationInstance -Identity $resourceAccount
+                $customProperties = @{
+                    AutoAttendant = $resourceAccountAA.Name
+                    DisplayName = $appInstance.DisplayName
+                    UserPrincipalName = $appInstance.UserPrincipalName
+                    LineUri = $appInstance.PhoneNumber
+                }
+                $assignedAccounts += New-Object -TypeName PSObject -Property $customProperties
+            }
+        } else {
+            Write-Host "Auto Attendant $($resourceAccountAA.Name) has no resource account assigned" -ForegroundColor Yellow
+        }
+    } else {
+        Write-Host "No Auto Attendants found with that name" -ForegroundColor Yellow
+    }
+} else {
+    $resourceAccountAAs = @()
+    $resourceAccountAAs = @(Get-CsAutoAttendant)
+    if ($resourceAccountAAs.Count -gt 1) {
+        foreach ($resourceAccountAA in $resourceAccountAAs) {
+            Write-Host "Auto Attendant $($resourceAccountAA.Name) found" -ForegroundColor Cyan
+            $resourceAccounts = $resourceAccountAA.ApplicationInstances
+            if ($resourceAccounts) {
+                foreach ($resourceAccount in $resourceAccounts) { 
+                    $appInstance = Get-CsOnlineApplicationInstance -Identity $resourceAccount
+                    $customProperties = @{
+                        AutoAttendant = $resourceAccountAA.Name
+                        DisplayName = $appInstance.DisplayName
+                        UserPrincipalName = $appInstance.UserPrincipalName
+                        LineUri = $appInstance.PhoneNumber
+                    }
+                    $assignedAccounts += New-Object -TypeName PSObject -Property $customProperties
+                }
+            } else {
+                Write-Host "Auto Attendant $($resourceAccountAA.Name) has no resource account assigned" -ForegroundColor Yellow
+            }
+        }
+    } elseif ($resourceAccountAAs.Count -eq 1) {
+        Write-Host "Auto Attendant $($resourceAccountAA.Name) found" -ForegroundColor Cyan
+        $resourceAccounts = $resourceAccountAA.ApplicationInstances
+        if ($resourceAccounts) {
+            foreach ($resourceAccount in $resourceAccounts) { 
+                $appInstance = Get-CsOnlineApplicationInstance -Identity $resourceAccount
+                $customProperties = @{
+                    AutoAttendant = $resourceAccountAA.Name
+                    DisplayName = $appInstance.DisplayName
+                    UserPrincipalName = $appInstance.UserPrincipalName
+                    LineUri = $appInstance.PhoneNumber
+                }
+                $assignedAccounts += New-Object -TypeName PSObject -Property $customProperties
+            }
+        } else {
+            Write-Host "Auto Attendant $($resourceAccountAA.Name) has no resource account assigned" -ForegroundColor Yellow
+        }
+    } else {
+        Write-Host "No Auto Attendants found" -ForegroundColor Yellow
+    }
+}
+$assignedAccounts
+}
