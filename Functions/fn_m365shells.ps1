fed0245b9f3c8ef54532657a5512a7d86b44a7b2
csatswo <68028099+csatswo@users.noreply.github.com>
2021-09-13 10:09:14 -0700

    Update fn_m365shells.ps1

--- Functions/fn_m365shells.ps1
+++ Functions/fn_m365shells.ps1
@@ -1,3 +1,11 @@
+Function msol {
+    Import-Module MSOnline;Connect-MsolService | Out-Null
+}
+
+Function aad {
+    Import-Module AzureADPreview;Connect-AzureAD | Out-Null
+}
+
 Function teams {
     Import-Module MicrosoftTeams;Connect-MicrosoftTeams| Out-Null
 }
@@ -6,14 +14,25 @@ Function exo {
     Import-Module ExchangeOnlineManagement;Connect-ExchangeOnline -ShowBanner:$false
 }
 
-Function aad {
-    Import-Module AzureADPreview;Connect-AzureAD | Out-Null
-}
-
-Function msol {
-    Import-Module MSOnline;Connect-MsolService | Out-Null
+Function m365mfa {
+    Write-Host "Connecting to MSOnline..."
+    msol
+    Write-Host "Connecting to AzureAD..."
+    aad
+    Write-Host "Connecting to Teams..."
+    teams
+    Write-Host "Connecting to ExchangeOnline..."
+    exo
 }
 
 Function m365 {
-    msol;aad;teams;exo
+    $Credential  = Get-Credential
+    Write-Host "Connecting to MSOnline..."
+    Import-Module MSOnline;Connect-MsolService -Credential $Credential | Out-Null
+    Write-Host "Connecting to AzureAD..."
+    Import-Module AzureADPreview;Connect-AzureAD -Credential $Credential | Out-Null
+    Write-Host "Connecting to Teams..."
+    Import-Module MicrosoftTeams;Connect-MicrosoftTeams -Credential $Credential | Out-Null
+    Write-Host "Connecting to ExchangeOnline..."
+    Import-Module ExchangeOnlineManagement;Connect-ExchangeOnline -Credential $Credential -ShowBanner:$false
 }
