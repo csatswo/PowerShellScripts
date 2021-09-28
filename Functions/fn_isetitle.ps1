b7ef73e7adedceb67b9882db42d0f174692bce96
csatswo <68028099+csatswo@users.noreply.github.com>
2021-09-28 10:06:21 -0700

    Updates

--- Functions/fn_isetitle.ps1
+++ Functions/fn_isetitle.ps1
@@ -1,4 +1,4 @@
 ﻿function iseTitle {
-    $Host.UI.RawUI.WindowTitle = "M365: $((Get-MsolDomain | Where-Object {$_.isDefault}).name)"
+    $Host.UI.RawUI.WindowTitle = (Get-MsolDomain | Where-Object {$_.isDefault}).name
     Set-Location C:\Temp
 }
---
+++ Functions/fn_servicenumbers.ps1
@@ -0,0 +1,31 @@
+﻿function ServiceNumbers {
+    $serviceNumbers = Get-CsOnlineTelephoneNumber | Where-Object {$_.InventoryType -ne "Subscriber"}
+    $customObject = @()
+    foreach ($serviceNumber in $serviceNumbers) {
+        if ($serviceNumber.TargetType -ne $null) {
+            $appId = Get-CsOnlineApplicationInstance | Where-Object {$_.PhoneNumber -like ("tel:+"+$serviceNumber.Id)}
+            $customProperties = @{
+                Id = $serviceNumber.Id
+                InventoryType = $serviceNumber.InventoryType
+                ActivationState = $serviceNumber.ActivationState
+                PortInOrderStatus = $serviceNumber.PortInOrderStatus
+                TargetType = $serviceNumber.TargetType
+                ResourceAccount = $appId.DisplayName
+                UserPrincipalName = $appId.UserPrincipalName
+            }
+        $customObject += New-Object -TypeName PSObject -Property $customProperties
+        } else {
+            $customProperties = @{
+                Id = $serviceNumber.Id
+                InventoryType = $serviceNumber.InventoryType
+                ActivationState = $serviceNumber.ActivationState
+                PortInOrderStatus = $serviceNumber.PortInOrderStatus
+                TargetType = $serviceNumber.TargetType
+                ResourceAccount = "N/A"
+                UserPrincipalName = "N/A"
+            }
+            $customObject += New-Object -TypeName PSObject -Property $customProperties
+        }
+    }
+    $customObject | Sort-Object InventoryType,Id | Select-Object Id,InventoryType,ActivationState,PortInOrderStatus,TargetType,ResourceAccount,UserPrincipalName | ft -AutoSize
+}
