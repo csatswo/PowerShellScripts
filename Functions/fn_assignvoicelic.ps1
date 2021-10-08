177f8f34e07b975f24762fc2c6b70ac167378329
csatswo <68028099+csatswo@users.noreply.github.com>
2021-10-08 12:48:08 -0700

    Updates and new functions

---
+++ Functions/fn_assignvoicelic.ps1
@@ -0,0 +1,37 @@
+﻿Function AssignPS {
+    [CmdletBinding()]Param(
+        [string]$UserPrincipalName
+    )
+    # Get licenses SKUs for Phone System
+    $msolAccountSkus = Get-MsolAccountSku
+    $phoneSystemSku = $msolAccountSkus | ? {$_.AccountSkuId -like "*MCOEV*"}
+    # Get number of available licenses
+    $availablePhoneSystem = $phoneSystemSku.ActiveUnits - $phoneSystemSku.ConsumedUnits
+    # Check the number of available Phone System licenses
+    if ($availablePhoneSystem -gt 0) { 
+        Write-Host "There are $availablePhoneSystem Phone System licenses available." -ForegroundColor Green
+        Write-Host "Assigning $($phoneSystemSku.AccountSkuId) to $(Get-CsOnlineUser -Identity $UserPrincipalName | Select-Object DisplayName)"
+        Set-MsolUserLicense -UserPrincipalName $UserPrincipalName -AddLicenses $phoneSystemSku.AccountSkuId
+    } else {
+        Write-Warning "Not enough available Phone System licenses."
+    }
+}
+
+Function AssignAC {
+    [CmdletBinding()]Param(
+        [string]$UserPrincipalName
+    )
+    # Get licenses SKUs for Phone System
+    $msolAccountSkus = Get-MsolAccountSku
+    $audioConfSku = $msolAccountSkus | ? {$_.AccountSkuId -like "*MCOMEETADV*"}
+    # Get number of available licenses
+    $availableAudioConf = $audioConfSku.ActiveUnits - $audioConfSku.ConsumedUnits
+    # Check the number of available Phone System licenses
+    if ($availableAudioConf -gt 0) { 
+        Write-Host "There are $availableAudioConf Audio Conferencing licenses available." -ForegroundColor Green
+        Write-Host "Assigning $($audioConfSku.AccountSkuId) to $(Get-CsOnlineUser -Identity $UserPrincipalName | Select-Object DisplayName)"
+        Set-MsolUserLicense -UserPrincipalName $UserPrincipalName -AddLicenses $audioConfSku.AccountSkuId
+    } else {
+        Write-Warning "Not enough available Audio Conferencing licenses."
+    }
+}
--- Functions/fn_m365shells.ps1
+++ Functions/fn_m365shells.ps1
@@ -14,6 +14,12 @@ Function exo {
     Import-Module ExchangeOnlineManagement;Connect-ExchangeOnline -ShowBanner:$false
 }
 
+Function spo {
+    $domain = ((Get-AzureADDomain | Where-Object {$_.Name -like "*.onmicrosoft.com" -and $_.Name -notlike "*.mail.onmicrosoft.com"}).Name) -replace "\.onmicrosoft\.com"
+    $spoAdminUrl = "https://"+$domain+"-admin.sharepoint.com"
+    Import-Module Microsoft.Online.SharePoint.PowerShell;Connect-SPOService -Url $spoAdminUrl
+}
+
 Function m365mfa {
     Write-Host "Connecting to MSOnline..."
     msol
