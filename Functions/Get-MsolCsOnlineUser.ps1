d7e336ef7a8d56f788b9a0ea6490646a064f627b
csatswo <68028099+csatswo@users.noreply.github.com>
2021-10-29 08:24:57 -0700

    Create Get-MsolCsOnlineUser.ps1

---
+++ Functions/Get-MsolCsOnlineUser.ps1
@@ -0,0 +1,8 @@
+﻿Function Get-MsolCsOnlineUser {
+    [CmdletBinding()]Param(
+        [string]$UserPrincipalName
+    )
+    $csOnlineMsolUser = New-Object -TypeName PSObject
+    (Get-CsOnlineUser -Identity $UserPrincipalName),(Get-MsolUser -UserPrincipalName $UserPrincipalName | Select-Object isLicensed,Licenses) | ForEach-Object {$msolUser = $_; $msolUser | Get-Member -MemberType Property,NoteProperty | ForEach-Object {$csOnlineMsolUser | Add-Member -NotePropertyMembers @{$_.Name=$msolUser.($_.Name)} -Force}}
+    $csOnlineMsolUser
+}
