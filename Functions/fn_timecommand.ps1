aee803633a19da4714631ca0c405212a2628c457
csatswo <68028099+csatswo@users.noreply.github.com>
2022-04-05 08:27:12 -0700

    Create fn_timecommand.ps1

---
+++ Functions/fn_timecommand.ps1
@@ -0,0 +1,14 @@
+﻿# Time a command and show in seconds
+Function  TimeCommand {
+    $cmd = ""
+    for ($i = 0; $i -lt $args.Count; $i++) {
+        $cmd += $args[$i]
+        if ($i -lt $args.Count -1) {
+            $cmd += " "
+        }
+    }
+    $start = Get-Date
+    Invoke-Expression -Command $cmd
+    $duration = (Get-Date) - $start
+    Write-Host "Command execution took $($duration.TotalSeconds) seconds" -ForegroundColor Yellow
+}
