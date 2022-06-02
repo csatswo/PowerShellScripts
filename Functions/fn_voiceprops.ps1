d84274193d7862fa373080c88d75b493174c5b5d
csatswo <68028099+csatswo@users.noreply.github.com>
2022-06-02 07:20:36 -0700

    Update fn_voiceprops.ps1

--- Functions/fn_voiceprops.ps1
+++ Functions/fn_voiceprops.ps1
@@ -5,25 +5,49 @@
     $userProperties = @()
     $msolUser = Get-MsolUser -UserPrincipalName $UserPrincipalName -ErrorAction SilentlyContinue
     $csOnlineUser = Get-CsOnlineUser -Identity $UserPrincipalName -ErrorAction SilentlyContinue
-    $customProperties = @{
-        DisplayName = $csOnlineUser.DisplayName
-        UserPrincipalName = $csOnlineUser.UserPrincipalName
-        SipAddress = $csOnlineUser.SipAddress
-        EnterpriseVoiceEnabled = $csOnlineUser.EnterpriseVoiceEnabled
-        OnPremLineURI = $csOnlineUser.OnPremLineURI
-        LineUri = $csOnlineUser.LineUri
-        OnlineVoiceRoutingPolicy = $csOnlineUser.OnlineVoiceRoutingPolicy
-        TenantDialPlan = $csOnlineUser.TenantDialPlan
-        TeamsCallingPolicy = $csOnlineUser.TeamsCallingPolicy
-        TeamsMeetingPolicy = $csOnlineUser.TeamsMeetingPolicy
-        TeamsMeetingBroadcastPolicy = $csOnlineUser.TeamsMeetingBroadcastPolicy
-        TeamsUpgradeEffectiveMode = $csOnlineUser.TeamsUpgradeEffectiveMode
-        RegistrarPool = $csOnlineUser.RegistrarPool
-        UsageLocation = $csOnlineUser.UsageLocation
-        isLicensed = $msolUser.isLicensed
-        Licenses = $msolUser.Licenses.AccountSkuId
-        ObjectId = $msolUser.ObjectId
-        }
-    $userProperties += New-Object -TypeName PSObject -Property $customProperties
-    $userProperties | Select-Object DisplayName,UserPrincipalName,SipAddress,EnterpriseVoiceEnabled,OnPremLineURI,LineUri,OnlineVoiceRoutingPolicy,TenantDialPlan,TeamsCallingPolicy,TeamsMeetingPolicy,TeamsMeetingBroadcastPolicy,TeamsUpgradeEffectiveMode,RegistrarPool,UsageLocation,isLicensed,Licenses,ObjectId
+    if ($msolUser.IsLicensed -eq $true) {
+        $customProperties = @{
+            DisplayName = $csOnlineUser.DisplayName
+            UserPrincipalName = $csOnlineUser.UserPrincipalName
+            SipAddress = $csOnlineUser.SipAddress
+            EnterpriseVoiceEnabled = $csOnlineUser.EnterpriseVoiceEnabled
+            OnPremLineURI = $csOnlineUser.OnPremLineURI
+            LineUri = $csOnlineUser.LineUri
+            OnlineVoiceRoutingPolicy = $csOnlineUser.OnlineVoiceRoutingPolicy
+            TenantDialPlan = $csOnlineUser.TenantDialPlan
+            TeamsCallingPolicy = $csOnlineUser.TeamsCallingPolicy
+            TeamsMeetingPolicy = $csOnlineUser.TeamsMeetingPolicy
+            TeamsMeetingBroadcastPolicy = $csOnlineUser.TeamsMeetingBroadcastPolicy
+            TeamsUpgradeEffectiveMode = $csOnlineUser.TeamsUpgradeEffectiveMode
+            RegistrarPool = $csOnlineUser.RegistrarPool
+            UsageLocation = $csOnlineUser.UsageLocation
+            isLicensed = $msolUser.isLicensed
+            Licenses = $msolUser.Licenses.AccountSkuId
+            ObjectId = $msolUser.ObjectId
+            }
+        $userProperties += New-Object -TypeName PSObject -Property $customProperties
+        $userProperties | Select-Object DisplayName,UserPrincipalName,SipAddress,EnterpriseVoiceEnabled,OnPremLineURI,LineUri,OnlineVoiceRoutingPolicy,TenantDialPlan,TeamsCallingPolicy,TeamsMeetingPolicy,TeamsMeetingBroadcastPolicy,TeamsUpgradeEffectiveMode,RegistrarPool,UsageLocation,isLicensed,Licenses,ObjectId
+    } else {
+        $customProperties = @{
+            DisplayName = $csOnlineUser.DisplayName
+            UserPrincipalName = $csOnlineUser.UserPrincipalName
+            SipAddress = $csOnlineUser.SipAddress
+            EnterpriseVoiceEnabled = $csOnlineUser.EnterpriseVoiceEnabled
+            OnPremLineURI = $csOnlineUser.OnPremLineURI
+            LineUri = $csOnlineUser.LineUri
+            OnlineVoiceRoutingPolicy = $csOnlineUser.OnlineVoiceRoutingPolicy
+            TenantDialPlan = $csOnlineUser.TenantDialPlan
+            TeamsCallingPolicy = $csOnlineUser.TeamsCallingPolicy
+            TeamsMeetingPolicy = $csOnlineUser.TeamsMeetingPolicy
+            TeamsMeetingBroadcastPolicy = $csOnlineUser.TeamsMeetingBroadcastPolicy
+            TeamsUpgradeEffectiveMode = $csOnlineUser.TeamsUpgradeEffectiveMode
+            RegistrarPool = $csOnlineUser.RegistrarPool
+            UsageLocation = $csOnlineUser.UsageLocation
+            isLicensed = $msolUser.isLicensed
+            Licenses = $null
+            ObjectId = $msolUser.ObjectId
+            }
+        $userProperties += New-Object -TypeName PSObject -Property $customProperties
+        $userProperties | Select-Object DisplayName,UserPrincipalName,SipAddress,EnterpriseVoiceEnabled,OnPremLineURI,LineUri,OnlineVoiceRoutingPolicy,TenantDialPlan,TeamsCallingPolicy,TeamsMeetingPolicy,TeamsMeetingBroadcastPolicy,TeamsUpgradeEffectiveMode,RegistrarPool,UsageLocation,isLicensed,Licenses,ObjectId
+    }
 }
