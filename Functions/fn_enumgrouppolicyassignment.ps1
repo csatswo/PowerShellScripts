15291ddb723fa3b910eabcf847fc5cf3d6abf199
csatswo <68028099+csatswo@users.noreply.github.com>
2022-07-26 23:41:37 -0700

    New functions

---
+++ Functions/fn_enumgrouppolicyassignment.ps1
@@ -0,0 +1,30 @@
+﻿Function EnumGroupPolicyAssignment {
+    $results = @()
+    $groupPolicyAssignments = Get-CsGroupPolicyAssignment
+    foreach ($groupPolicyAssignment in $groupPolicyAssignments) {
+        $group = Get-MsolGroup -ObjectId $groupPolicyAssignment.GroupId
+        $groupMembers = Get-MsolGroupMember -GroupObjectId $groupPolicyAssignment.GroupId
+        if ($groupMembers) {
+            foreach ($groupMember in $groupMembers) {
+                $results += [PSCustomObject]@{
+                    PolicyType = $groupPolicyAssignment.PolicyType
+                    PolicyName = $groupPolicyAssignment.PolicyName
+                    Rank = $groupPolicyAssignment.Priority
+                    Group = $group.DisplayName
+                    DisplayName = $groupMember.DisplayName
+                    UserPrincipalName = $groupMember.EmailAddress
+                }
+            }
+        } else {
+            $results += [PSCustomObject]@{
+                PolicyType = $groupPolicyAssignment.PolicyType
+                PolicyName = $groupPolicyAssignment.PolicyName
+                Rank = $groupPolicyAssignment.Priority
+                Group = $group.DisplayName
+                DisplayName = $null
+                UserPrincipalName = $null
+            }   
+        }
+    }
+    $results | Sort-Object PolicyType,Rank,UserPrincipalName | Select-Object PolicyType,PolicyName,Rank,Group,DisplayName,UserPrincipalName
+}
---
+++ Functions/fn_teamslicensestatus.ps1
@@ -0,0 +1,37 @@
+﻿function TeamsLicenseStatus {
+    [CmdletBinding()]Param(
+        [Parameter(mandatory=$true)][String]$UserPrincipalName
+    )
+    $msolUser = Get-MsolUser -UserPrincipalName $UserPrincipalName
+    if ($msolUser.IsLicensed -eq $true) {
+        $teamsLicense = foreach ($servicePlan in $msolUser.Licenses.ServiceStatus) { if ($servicePlan.ServicePlan.ServiceName -eq "TEAMS1") { $servicePlan } }
+        if ($teamsLicense) {
+            [PSCustomObject]@{
+                DisplayName = $msolUser.DisplayName
+                UserPrincipalName = $msolUser.UserPrincipalName
+                IsLicensed = $msolUser.IsLicensed
+                ServicePlan = $teamsLicense.ServicePlan.ServiceName
+                ProvisioningStatus = $teamsLicense.ProvisioningStatus
+                AssignedSkus = ($msolUser.Licenses.AccountSku.SkuPartNumber | Sort-Object) -join ","
+            }
+        } else {
+            [PSCustomObject]@{
+                DisplayName = $msolUser.DisplayName
+                UserPrincipalName = $msolUser.UserPrincipalName
+                IsLicensed = $msolUser.IsLicensed
+                ServicePlan = $null
+                ProvisioningStatus = $null
+                AssignedSkus = ($msolUser.Licenses.AccountSku.SkuPartNumber | Sort-Object) -join ","
+            }
+        }
+    } else {
+        [PSCustomObject]@{
+            DisplayName = $msolUser.DisplayName
+            UserPrincipalName = $msolUser.UserPrincipalName
+            IsLicensed = $msolUser.IsLicensed
+            ServicePlan = $null
+            ProvisioningStatus = $null
+            AssignedSkus = $null
+        }
+    }
+}
