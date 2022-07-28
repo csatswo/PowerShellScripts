6a659f4af6c4ef8ffc061efad8759eee726319dc
csatswo <68028099+csatswo@users.noreply.github.com>
2022-07-27 21:25:39 -0700

    Create fn_smtpaddressdump.ps1

---
+++ Functions/fn_smtpaddressdump.ps1
@@ -0,0 +1,15 @@
+﻿function SMTPAddressDump {
+    [CmdletBinding()]Param(
+        [Parameter(mandatory=$true)][String]$UserPrincipalName
+    )
+    $msolUser = Get-MsolUser -UserPrincipalName $UserPrincipalName
+    $proxyAddresses = $msolUser.ProxyAddresses
+    $primarySMTP = (($proxyAddresses | ? {$_ -clike "SMTP*"}) -replace "SMTP:")
+    $otherSMTP = (($proxyAddresses | ? {$_ -clike "smtp*"}) -replace "smtp:")
+    $results += [PSCustomObject]@{
+        UserPrincipalName = $msolUser.UserPrincipalName
+        PrimarySMTP = $primarySMTP
+        OtherSMTP = $otherSMTP
+        }
+    $results
+}
