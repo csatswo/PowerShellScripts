4f5d777b7b8435c7763e36da7e3de53c06cfc476
csatswo <68028099+csatswo@users.noreply.github.com>
2022-07-31 06:25:56 -0700

    Update fn_smtpaddressdump.ps1

--- Functions/fn_smtpaddressdump.ps1
+++ Functions/fn_smtpaddressdump.ps1
@@ -1,11 +1,18 @@
 ﻿function SMTPAddressDump {
     [CmdletBinding()]Param(
-        [Parameter(mandatory=$true)][String]$UserPrincipalName
+        [Parameter(mandatory=$true)][String]$UserPrincipalName,
+        [Parameter(mandatory=$false)][Bool]$Join
     )
     $msolUser = Get-MsolUser -UserPrincipalName $UserPrincipalName
     $proxyAddresses = $msolUser.ProxyAddresses
     $primarySMTP = (($proxyAddresses | ? {$_ -clike "SMTP*"}) -replace "SMTP:")
-    $otherSMTP = (($proxyAddresses | ? {$_ -clike "smtp*"}) -replace "smtp:")
+    if ($Join -eq $true) {
+        Write-Host "Enter the join character(s): " -ForegroundColor Cyan -NoNewline
+        $joinChars = Read-Host
+        $otherSMTP = ((($proxyAddresses | ? {$_ -clike "smtp*"}) -replace "smtp:" | Sort-Object) -join "$joinChars")
+    } else {
+        $otherSMTP = (($proxyAddresses | ? {$_ -clike "smtp*"}) -replace "smtp:" | Sort-Object)
+    }
     $results += [PSCustomObject]@{
         UserPrincipalName = $msolUser.UserPrincipalName
         PrimarySMTP = $primarySMTP
