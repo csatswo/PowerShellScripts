49e2d2714e66411b4548d25a57c41c5f94227d46
csatswo <68028099+csatswo@users.noreply.github.com>
2022-08-09 14:35:05 -0700

    Update fn_voiceprops.ps1

--- Functions/fn_voiceprops.ps1
+++ Functions/fn_voiceprops.ps1
@@ -4,39 +4,43 @@
         [Parameter(mandatory=$false)][Bool]$Join
     )
     $userProperties = @()
-    $msolUser = Get-MsolUser -UserPrincipalName $UserPrincipalName -ErrorAction SilentlyContinue
-    $csOnlineUser = Get-CsOnlineUser -Identity $UserPrincipalName -ErrorAction SilentlyContinue
-    $groupPolicyAssignments = EnumGroupPolicyAssignment
-    $userGroupPolicyAssignments = $groupPolicyAssignments | ? {$_.UserPrincipalName -eq "$UserPrincipalName"} | Sort-Object
-    if ($msolUser.IsLicensed -eq $true) {
-        $licenses = $msolUser.Licenses.AccountSkuId
-    } else {
-        $licenses = $null
+    try {
+        $msolUser = Get-MsolUser -UserPrincipalName $UserPrincipalName -ErrorAction Stop
+        $csOnlineUser = Get-CsOnlineUser -Identity $UserPrincipalName -ErrorAction Stop
+        $groupPolicyAssignments = EnumGroupPolicyAssignment
+        $userGroupPolicyAssignments = $groupPolicyAssignments | ? {$_.UserPrincipalName -eq "$UserPrincipalName"} | Sort-Object
+        if ($msolUser.IsLicensed -eq $true) {
+            $licenses = $msolUser.Licenses.AccountSkuId
+        } else {
+            $licenses = $null
+        }
+        if ($Join -eq $true) {
+            Write-Host "Enter the join character(s): " -ForegroundColor Cyan -NoNewline
+            $joinChars = Read-Host
+            $licenses = (($licenses | Sort-Object) -join "$joinChars")
+        }
+        $userProperties = [PSCustomObject]@{
+            DisplayName = $csOnlineUser.DisplayName
+            UserPrincipalName = $csOnlineUser.UserPrincipalName
+            SipAddress = $csOnlineUser.SipAddress
+            EnterpriseVoiceEnabled = $csOnlineUser.EnterpriseVoiceEnabled
+            OnPremLineURI = $csOnlineUser.OnPremLineURI
+            LineUri = $csOnlineUser.LineUri
+            OnlineVoiceRoutingPolicy = $csOnlineUser.OnlineVoiceRoutingPolicy
+            TenantDialPlan = $csOnlineUser.TenantDialPlan
+            TeamsCallingPolicy = $csOnlineUser.TeamsCallingPolicy
+            TeamsMeetingPolicy = $csOnlineUser.TeamsMeetingPolicy
+            TeamsMeetingBroadcastPolicy = $csOnlineUser.TeamsMeetingBroadcastPolicy
+            TeamsUpgradeEffectiveMode = $csOnlineUser.TeamsUpgradeEffectiveMode
+            RegistrarPool = $csOnlineUser.RegistrarPool
+            UsageLocation = $csOnlineUser.UsageLocation
+            isLicensed = $msolUser.isLicensed
+            Licenses = $licenses
+            ObjectId = $msolUser.ObjectId
+            GroupPolicyAssignments = $userGroupPolicyAssignments
+        }
+        $userProperties | Select-Object DisplayName,UserPrincipalName,SipAddress,EnterpriseVoiceEnabled,OnPremLineURI,LineUri,OnlineVoiceRoutingPolicy,TenantDialPlan,TeamsCallingPolicy,TeamsMeetingPolicy,TeamsMeetingBroadcastPolicy,TeamsUpgradeEffectiveMode,RegistrarPool,UsageLocation,isLicensed,Licenses,ObjectId,GroupPolicyAssignments
+    } catch {
+        Write-Host "$($_.Exception)" -ForegroundColor Red
     }
-    if ($Join -eq $true) {
-        Write-Host "Enter the join character(s): " -ForegroundColor Cyan -NoNewline
-        $joinChars = Read-Host
-        $licenses = (($licenses | Sort-Object) -join "$joinChars")
-    }
-    $userProperties = [PSCustomObject]@{
-        DisplayName = $csOnlineUser.DisplayName
-        UserPrincipalName = $csOnlineUser.UserPrincipalName
-        SipAddress = $csOnlineUser.SipAddress
-        EnterpriseVoiceEnabled = $csOnlineUser.EnterpriseVoiceEnabled
-        OnPremLineURI = $csOnlineUser.OnPremLineURI
-        LineUri = $csOnlineUser.LineUri
-        OnlineVoiceRoutingPolicy = $csOnlineUser.OnlineVoiceRoutingPolicy
-        TenantDialPlan = $csOnlineUser.TenantDialPlan
-        TeamsCallingPolicy = $csOnlineUser.TeamsCallingPolicy
-        TeamsMeetingPolicy = $csOnlineUser.TeamsMeetingPolicy
-        TeamsMeetingBroadcastPolicy = $csOnlineUser.TeamsMeetingBroadcastPolicy
-        TeamsUpgradeEffectiveMode = $csOnlineUser.TeamsUpgradeEffectiveMode
-        RegistrarPool = $csOnlineUser.RegistrarPool
-        UsageLocation = $csOnlineUser.UsageLocation
-        isLicensed = $msolUser.isLicensed
-        Licenses = $licenses
-        ObjectId = $msolUser.ObjectId
-        GroupPolicyAssignments = $userGroupPolicyAssignments
-    }
-    $userProperties | Select-Object DisplayName,UserPrincipalName,SipAddress,EnterpriseVoiceEnabled,OnPremLineURI,LineUri,OnlineVoiceRoutingPolicy,TenantDialPlan,TeamsCallingPolicy,TeamsMeetingPolicy,TeamsMeetingBroadcastPolicy,TeamsUpgradeEffectiveMode,RegistrarPool,UsageLocation,isLicensed,Licenses,ObjectId,GroupPolicyAssignments
 }
