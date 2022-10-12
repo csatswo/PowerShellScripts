7eee9f85185aef33e7aa136967e81c9ba15b4bc1
csatswo <68028099+csatswo@users.noreply.github.com>
2022-10-12 10:04:47 -0700

    Update fn_shrinkvhd.ps1

--- Functions/fn_shrinkvhd.ps1
+++ Functions/fn_shrinkvhd.ps1
@@ -1,29 +1,36 @@
 Function shrinkvhd {
-    $driveUsageBefore = 0
-    $driveUsageAfter = 0
+    Clear-Host
+    $totalDriveUsageBefore = 0
+    $totalDriveUsageAfter = 0
     $virtualMachines = Get-VM
     $VHDs = $virtualMachines.HardDrives.Path
-    $runningVMs = $virtualMachines| Where-Object {$_.State -eq "Running"}
-    Write-Host `n`n`n`n`n`n
-    if ($runningVMs) {
-        Write-Host "The following VMs are still running:`n" -ForegroundColor Yellow
-        Write-Output $runningVMs.Name
-        Read-Host -Prompt "`nPress Enter to turn off, or Ctrl-C to cancel"
-        foreach ($VM in $runningVMs) {
-            Write-Host "Stopping $($VM.Name)..."
-            Stop-VM -Name $VM.Name
+    $VHDs | ForEach-Object {$totalDriveUsageBefore += ((Measure-Object -InputObject (Get-Item $_).Length -Sum).Sum / 1GB)}
+    foreach ($virtualMachine in $virtualMachines) {
+        Write-Host "Shrinking $($virtualMachine.Name)" -ForegroundColor Yellow
+        if ($virtualMachine.State -eq "Running") {
+            $runningPrompt = Read-Host -Prompt "The VM is still running. Enter `"Y`" to turn off, or anything else to skip."
+            if ($runningPrompt -eq "y") {
+                Write-Host "Stopping $($virtualMachine.Name)..."
+                Stop-VM -Name $virtualMachine.Name
+            }
         }
+        foreach ($VHD in $($virtualMachine.HardDrives.Path)) {
+            $driveUsageBefore = 0
+            $driveUsageAfter = 0
+            $driveUsageBefore += ((Measure-Object -InputObject (Get-Item $VHD).Length -Sum).Sum / 1GB)
+            Write-Host "    Shrinking $VHD" -ForegroundColor Yellow
+            Mount-VHD -Path $VHD -NoDriveLetter -ReadOnly
+            Optimize-VHD -Path $VHD -Mode Full
+            Optimize-VHD -Path $VHD -Mode Full
+            Optimize-VHD -Path $VHD -Mode Full
+            Dismount-VHD -Path $VHD
+            $driveUsageAfter += ((Measure-Object -InputObject (Get-Item $VHD).Length -Sum).Sum / 1GB)
+            Write-Host "    Drive before: $driveUsageBefore GB`n    Drive after: $driveUsageAfter GB"
+        }
+        
     }
-    $VHDs | foreach {$driveUsageBefore += ((Measure-Object -InputObject (Get-Item $_).Length -Sum).Sum / 1GB)}
-    foreach ($VHD in $VHDs) {
-        Write-Host "Shrinking $VHD" -ForegroundColor Yellow
-        Mount-VHD -Path $VHD -NoDriveLetter -ReadOnly
-        Optimize-VHD -Path $VHD -Mode Full
-        Optimize-VHD -Path $VHD -Mode Full
-        Optimize-VHD -Path $VHD -Mode Full
-        Dismount-VHD -Path $VHD
-    }
-    $VHDs | foreach {$driveUsageAfter += ((Measure-Object -InputObject (Get-Item $_).Length -Sum).Sum / 1GB)}
-    Write-Host "Drive usage before was $driveUsageBefore GB`nDrive after before was $driveUsageAfter GB"
-    Write-Host "`nTotal saved: $($driveUsageBefore - $driveUsageAfter) GB"
+    $VHDs | ForEach-Object {$totalDriveUsageAfter += ((Measure-Object -InputObject (Get-Item $_).Length -Sum).Sum / 1GB)}
+    Write-Host "Shrinking complete" -ForegroundColor Yellow
+    Write-Host "Drive usage before was $totalDriveUsageBefore GB`nDrive after before was $totalDriveUsageAfter GB"
+    Write-Host "Total saved: $($totalDriveUsageBefore - $totalDriveUsageAfter) GB"
 }
