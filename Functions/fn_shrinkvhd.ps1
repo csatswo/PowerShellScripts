9ace7916b1d12e7d02c74ae7f1ddca76ae045c12
Chris <68028099+csatswo@users.noreply.github.com>
2022-12-06 10:18:40 -0800

    Update fn_shrinkvhd.ps1

--- Functions/fn_shrinkvhd.ps1
+++ Functions/fn_shrinkvhd.ps1
@@ -4,7 +4,15 @@ Function shrinkvhd {
     $virtualMachines = Get-VM
     $VHDs = $virtualMachines.HardDrives.Path
     $VHDs | ForEach-Object {$totalDriveUsageBefore += ((Measure-Object -InputObject (Get-Item $_).Length -Sum).Sum / 1GB)}
-    Write-Host `n`n`n`n`n
+    $allVHDAbsolutePaths = @()
+    foreach ($absoultePath in $virtualMachines.HardDrives.Path) {
+        $allVHDAbsolutePaths += Get-Item $absoultePath | Select-Object DirectoryName
+    }
+    $allVHDDirectories = $allVHDAbsolutePaths | Select-Object -Unique
+    $allUnattachedVHDs = @()
+    foreach ($directoryName in $allVHDDirectories.DirectoryName) {
+        $allUnattachedVHDs += Get-ChildItem -Path $directoryName -Recurse | Where-Object {$_.Extension -eq ".vhdx"} | Where-Object -FilterScript { $_.FullName -notin $VHDs } | Select-Object FullName
+    }
     foreach ($virtualMachine in $virtualMachines) {
         Write-Host "Shrinking $($virtualMachine.Name)" -ForegroundColor Cyan
         if ($virtualMachine.HardDrives) {
@@ -41,6 +49,26 @@ Function shrinkvhd {
             }
         } else {
             Write-Host "    No drive attached to $($virtualMachine.Name)" -ForegroundColor Red
+        }
+    }
+    if ($allUnattachedVHDs) {
+        Write-Host "Found unattached VHDs:" -ForegroundColor Magenta
+        foreach ($unattachedVHD in $allUnattachedVHDs) {
+            Write-Host "$($unattachedVHD.FullName)"
+        }
+        foreach ($unattachedVHD in $allUnattachedVHDs) {
+            $driveUsageBefore = 0
+            $driveUsageAfter = 0
+            $driveUsageBefore += ((Measure-Object -InputObject (Get-Item ($unattachedVHD.FullName)).Length -Sum).Sum / 1GB)
+            Write-Host "    Shrinking $($unattachedVHD.FullName)" -ForegroundColor Yellow
+            Mount-VHD -Path $($unattachedVHD.FullName) -NoDriveLetter -ReadOnly
+            Optimize-VHD -Path $($unattachedVHD.FullName) -Mode Full
+            Optimize-VHD -Path $($unattachedVHD.FullName) -Mode Full
+            Optimize-VHD -Path $($unattachedVHD.FullName) -Mode Full
+            Dismount-VHD -Path $($unattachedVHD.FullName)
+            $driveUsageAfter += ((Measure-Object -InputObject (Get-Item $($unattachedVHD.FullName)).Length -Sum).Sum / 1GB)
+            Write-Host "    Before: $([math]::Round($driveUsageBefore,2)) GB`n    After:  $([math]::Round($driveUsageAfter,2)) GB"
+            Write-Host "    Shrunk: $([math]::Round(($driveUsageBefore - $driveUsageAfter),2)) GB"
         }
     }
     $VHDs | ForEach-Object {$totalDriveUsageAfter += ((Measure-Object -InputObject (Get-Item $_).Length -Sum).Sum / 1GB)}
