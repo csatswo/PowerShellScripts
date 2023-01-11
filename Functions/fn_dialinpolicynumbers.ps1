c4f8cdddd3b3a0ff6cb71a5651d0d90da8bdd0dd
Chris <68028099+csatswo@users.noreply.github.com>
2023-01-10 17:27:23 -0800

    Create fn_dialinpolicynumbers.ps1

---
+++ Functions/fn_dialinpolicynumbers.ps1
@@ -0,0 +1,22 @@
+﻿Function DialinPolicyNumbers {
+    $results = @()
+    $confPolicies = Get-CsTeamsAudioConferencingPolicy
+    foreach ($policy in $confPolicies) {
+        if (-not ($policy.MeetingInvitePhoneNumbers)) {
+            $results += [PSCustomObject]@{
+                Policy = $policy.Identity
+                Number = $null
+                Index = $null
+            }        
+        } else {
+            foreach ($phoneNumber in $policy.MeetingInvitePhoneNumbers) {
+                $results += [PSCustomObject]@{
+                    Policy = $policy.Identity
+                    Number = [string]$phoneNumber
+                    Index = [int]$policy.MeetingInvitePhoneNumbers.IndexOf($phoneNumber)
+                }
+            }
+        }
+    }
+    $results
+}
