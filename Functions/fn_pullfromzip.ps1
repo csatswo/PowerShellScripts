9990674b9321e8cbaa44d895c8a67fba19c96d65
Chris <68028099+csatswo@users.noreply.github.com>
2023-02-01 15:29:14 -0800

    Create fn_pullfromzip.ps1

---
+++ Functions/fn_pullfromzip.ps1
@@ -0,0 +1,15 @@
+﻿Function PullFromZip {
+    [cmdletbinding()]
+    param(
+        [parameter(Mandatory=$true)][string]$FilesToExtract,
+        [parameter(Mandatory=$true)][string]$ZipFile
+    )
+    $f = Get-ChildItem -Path $ZipFile
+    Add-Type -AssemblyName "System.IO.Compression.FileSystem"
+    $zip = [IO.Compression.ZipFile]::OpenRead($ZipPath)
+    $zip.Entries | Where-Object {$_.Name -like "$FilesToExtract"} | ForEach-Object { 
+        $fileName = $_.Name
+        [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$($f.Directory)\$fileName", $true)
+    }
+    $zip.Dispose()
+}
