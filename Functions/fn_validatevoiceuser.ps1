a3558d2f6b4748da196fadbd311310f167549a82
Chris <68028099+csatswo@users.noreply.github.com>
2023-02-08 12:46:45 -0800

    Create fn_validatevoiceuser.ps1

---
+++ Functions/fn_validatevoiceuser.ps1
@@ -0,0 +1,45 @@
+﻿Function ValidateVoiceUser {
+    [cmdletbinding()]
+    param(
+        [parameter(Mandatory=$true,ValueFromPipeline=$true)]$UserPrincipalName
+    )
+    Process {
+        try {
+            $csUser = Get-CsOnlineUser -Identity $UserPrincipalName -ErrorAction SilentlyContinue
+            if ($csUser) {
+                if (-not ($csUser.AccountEnabled)) {
+                    Write-Verbose "$($csUser.UserPrincipalName) is not enabled"
+                    $validVoiceUser = $false
+                }
+                elseif (-not ($csUser.AssignedPlan)) {
+                    Write-Verbose "$($csUser.UserPrincipalName) is not licensed properly"
+                    $validVoiceUser = $false
+                }
+                elseif (-not ($csUser.AssignedPlan.Capability -contains "Teams")) {
+                    Write-Verbose "$($csUser.UserPrincipalName) is not licensed properly"
+                    $validVoiceUser = $false
+                }
+                else {
+                    $userLicenses = @{}
+                    foreach ($assignedPlan in $csUser.AssignedPlan) { $userLicenses.Add($assignedPlan.Capability,$assignedPlan) }
+                    if (($userLicenses["Teams"]).CapabilityStatus -ne "Enabled") {
+                        Write-Verbose "$($csUser.UserPrincipalName) is not licensed properly"
+                        $validVoiceUser = $false
+                    }
+                    else {
+                        $validVoiceUser = $true
+                    }
+                }
+            }
+            else {
+                Write-Verbose $($Error[0].Exception)
+                $validVoiceUser = $false
+            }
+        } catch {
+            $($Error[0].Exception)
+        }
+    }
+    End {
+        Return $validVoiceUser
+    }
+}
