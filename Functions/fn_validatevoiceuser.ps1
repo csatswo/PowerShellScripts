6ad632bdd0a4db02fea42cf97d9ccab8c344f8db
Chris <68028099+csatswo@users.noreply.github.com>
2023-02-08 18:34:33 -0800

    Update fn_validatevoiceuser.ps1

--- Functions/fn_validatevoiceuser.ps1
+++ Functions/fn_validatevoiceuser.ps1
@@ -12,21 +12,49 @@
                     $validVoiceUser = $false
                 }
                 elseif (-not ($csUser.AssignedPlan)) {
-                    Write-Verbose "$($csUser.UserPrincipalName) is not licensed properly"
-                    $validVoiceUser = $false
-                }
-                elseif (-not ($csUser.AssignedPlan.Capability -contains "Teams")) {
-                    Write-Verbose "$($csUser.UserPrincipalName) is not licensed properly"
+                    Write-Verbose "$($csUser.UserPrincipalName) is not licensed"
                     $validVoiceUser = $false
                 }
                 else {
                     $userLicenses = @{}
                     foreach ($assignedPlan in $csUser.AssignedPlan) { $userLicenses.Add($assignedPlan.Capability,$assignedPlan) }
-                    if (($userLicenses["Teams"]).CapabilityStatus -ne "Enabled") {
-                        Write-Verbose "$($csUser.UserPrincipalName) is not licensed properly"
+                    if ($userLicenses.Keys -contains "MCOEV_VIRTUALUSER" -and $true -eq ($userLicenses["MCOEV_VIRTUALUSER"]).CapabilityStatus) {
+                        # Licensed for Teams Virtual User
+                        Write-Verbose "$($csUser.UserPrincipalName) is licensed as Resource Account"
+                        $validVoiceUser = $true
+                    }
+                    elseif ($userLicenses.Keys -notcontains "Teams") {
+                        # Not licensed for Teams
+                        Write-Verbose "$($csUser.UserPrincipalName) is not licensed for Teams"
                         $validVoiceUser = $false
                     }
+                    elseif ($userLicenses.Keys -notcontains "MCOEV") {
+                        # Licensed for Teams but not Phone System
+                        Write-Verbose "$($csUser.UserPrincipalName) is not licensed for Phone System"
+                        $validVoiceUser = $false
+                    }
+                    elseif (-not (($userLicenses["Teams"]).CapabilityStatus -eq "Enabled" -and ($userLicenses["MCOEV"]).CapabilityStatus -eq "Enabled")) {
+                        $teamsLicense = (($userLicenses["Teams"]).CapabilityStatus -eq "Enabled")
+                        $voiceLicense = (($userLicenses["MCOEV"]).CapabilityStatus -eq "Enabled")
+                        if ($false -eq $teamsLicense -and $false -eq $voiceLicense) {
+                            # Licenses were removed
+                            Write-Verbose "$($csUser.UserPrincipalName) is not licensed for Teams or Phone System"
+                            $validVoiceUser = $false
+                        }
+                        if ($true -eq $teamsLicense -and $false -eq $voiceLicense) {
+                            # License for Phone System was removed
+                            Write-Verbose "$($csUser.UserPrincipalName) is not licensed for Phone System"
+                            $validVoiceUser = $false
+                        }
+                        if ($false -eq $teamsLicense -and $true -eq $voiceLicense) {
+                            # License for Teams was removed
+                            Write-Verbose "$($csUser.UserPrincipalName) is not licensed for Teams"
+                            $validVoiceUser = $false
+                        }
+                    }
                     else {
+                        # Licensed for Teams and Phone System
+                        Write-Verbose "$($csUser.UserPrincipalName) is licensed as Teams voice user"
                         $validVoiceUser = $true
                     }
                 }
