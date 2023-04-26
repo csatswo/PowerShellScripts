8a926cfff3634e33c1b64ee92f6f3f5057a2282f
Chris <68028099+csatswo@users.noreply.github.com>
2023-04-26 10:57:01 -0700

    Create fn_dumbstuff.ps1

---
+++ Functions/fn_dumbstuff.ps1
@@ -0,0 +1,8 @@
+﻿Function FirstLetters {
+    [CmdletBinding()]Param([string]$String)
+    $firstLetters = @()
+    foreach ($wordStr in (($String.ToLower() -split " ") -replace "\W")) {
+        $firstLetters += $wordStr.Substring(0,1)
+    }
+    $firstLetters -join ""
+}
