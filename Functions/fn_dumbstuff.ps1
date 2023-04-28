510dbf4d0a0dc762a12cdeaf812935a2b933502e
Chris <68028099+csatswo@users.noreply.github.com>
2023-04-27 20:03:26 -0700

    Update fn_dumbstuff.ps1

--- Functions/fn_dumbstuff.ps1
+++ Functions/fn_dumbstuff.ps1
@@ -6,3 +6,11 @@
     }
     $firstLetters -join ""
 }
+
+function DLTeamsNetAssess {
+    [CmdletBinding()]Param([Parameter(Mandatory=$true)][ValidateScript({ if (Test-Path $_){ $true } else { throw "Path $_ is not valid" } })][string]$Folder)
+    Write-Output "Downloading the Microsoft Teams Network Assessment Tool..."
+    $downloadPage = Invoke-WebRequest -UseBasicParsing -Uri 'https://www.microsoft.com/en-us/download/confirmation.aspx?id=103017'
+    $downloadLink = ($DownloadPage.Links | Where-Object {$_.href -like '*MicrosoftTeamsNetworkAssessmentTool.exe'}).href[0]
+    Invoke-WebRequest -UseBasicParsing -Uri $downloadLink -OutFile $Folder\MicrosoftTeamsNetworkAssessmentTool.exe
+}
