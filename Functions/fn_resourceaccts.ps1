ccefaeb853522723d33b1dfeaa0d9ab826caf0e4
Chris <68028099+csatswo@users.noreply.github.com>
2023-05-16 15:49:54 -0700

    Create fn_resourceaccts.ps1

---
+++ Functions/fn_resourceaccts.ps1
@@ -0,0 +1,22 @@
+﻿Function ResourceAccts {
+    $results = @()
+    $date = Get-Date -Format yyyy-MM-dd
+    $applicationEndpoints = Get-CsOnlineApplicationInstance
+    foreach ($applicationEndpoint in $applicationEndpoints) {
+        $csOnlineUser = Get-CsOnlineUser -Identity $applicationEndpoint.UserPrincipalName
+        if ($csOnlineUser.AssignedPlan) {
+            $assignedPlans = $csOnlineUser.AssignedPlan
+        }
+        else {
+            $assignedPlans = $null
+        }
+        $results += [PSCustomObject]@{
+            DisplayName        = $applicationEndpoint.DisplayName
+            UserPrincipalName  = $applicationEndpoint.UserPrincipalName
+            PhoneNumber        = $applicationEndpoint.PhoneNumber
+            ApplicationId      = $applicationEndpoint.ApplicationId
+            AssignedPlans      = $assignedPlans
+        }
+    }
+    $results
+}
