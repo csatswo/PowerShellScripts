c2e79ff2a71ce4a23b7f589224d54afae89bc60c
Chris <68028099+csatswo@users.noreply.github.com>
2023-06-29 08:15:45 -0700

    new functions

---
+++ Functions/fn_callflows.ps1
@@ -0,0 +1,60 @@
+﻿Function OutputMenu {
+    [CmdletBinding()]Param ([Parameter(Mandatory = $true,Position = 0,ValueFromPipeline = $true)][PSObject[]]$MenuOptions)
+    $menu = @()
+    foreach ($menuOption in ($menuOptions)) {
+        if ($menuOption.Action -eq "Announcement") {
+            if ($menuOption.Prompt.ActiveType -eq "TextToSpeech") { $target = ("TextToSpeech: " + $menuOption.Prompt.TextToSpeechPrompt) }
+            elseif ($menuOption.Prompt.ActiveType -eq "AudioFile") { $target = ("AudioFile: " + $menuOption.Prompt.AudioFilePrompt) }
+        }
+        elseif ($menuOption.Action -eq "DisconnectCall") { $target = "Disconnect" }
+        elseif ($menuOption.Action -eq "TransferCallToOperator") { $target = $operator }
+        elseif ($menuOption.Action -eq "TransferCallToTarget") {
+            # 
+            # The New-CsAutoAttendantCallableEntity cmdlet lets you create a callable entity for use with call transfers from the Auto Attendant service.
+            # Callable entities can be created using either Object ID or TEL URIs and can refer to any of the following entities:
+            # 
+            # User
+            # ApplicationEndpoint
+            # ExternalPstn
+            # SharedVoicemail
+            # 
+            if ($menuOption.CallTarget.Type -eq "User") { $target = ("User: " + (Get-CsOnlineUser -Identity $menuOption.CallTarget.Id).UserPrincipalName) }
+            elseif ($menuOption.CallTarget.Type -eq "ApplicationEndpoint") {
+                $appInstance = (Get-CsOnlineApplicationInstance -Identity $menuOption.CallTarget.Id)
+                if ($raApplicationIds[$appInstance.ApplicationId]) {
+                    $raAssociation = Get-CsOnlineApplicationInstanceAssociation -Identity $appInstance.ObjectId
+                    $hashMatch = $acdHash[$raAssociation.ConfigurationId]
+                    if ($raAssociation.ConfigurationType -eq "CallQueue") { $target = ("CallQueue: " + $hashMatch.Name) }
+                    else { $target = ("AutoAttendant: " + $hashMatch.Name) }
+                }
+                else { $target = ("ResourceAccount: " + (Get-CsOnlineApplicationInstance -Identity $menuOption.CallTarget.Id).UserPrincipalName) } 
+            }
+            elseif ($menuOption.CallTarget.Type -eq "ExternalPstn") { $target = ("ExternalPSTN: " + $menuOption.CallTarget.Id) }
+            elseif ($menuOption.CallTarget.Type -eq "SharedVoicemail") { $target = ("Voicemail: " + (Find-CsGroup -SearchQuery $menuOption.CallTarget.Id).DisplayName) }
+        }
+        $menu += [PSCustomObject]@{
+            DtmfResponse = $menuOption.DtmfResponse
+            Action       = $menuOption.Action
+            Target       = $target
+        }
+    }
+    $menu
+}
+
+Function OutputGreetings {
+    [CmdletBinding()]Param ([Parameter(Mandatory = $true,Position = 0,ValueFromPipeline = $true)][PSObject[]]$Menu)
+    $greetings = New-Object -TypeName "System.Collections.ArrayList"
+    $Menu.Greetings | foreach {
+    if ($_.ActiveType -eq "TextToSpeech") { $greeting = ("TextToSpeech: " + $_.TextToSpeechPrompt) }
+        elseif ($_ -eq "AudioFile") { $greeting = ("AudioFile: " + $_.AudioFilePrompt) }
+        $item = [PSCustomObject]@{Type='Greeting';Greeting=$greeting}
+        [void]$greetings.Add($item)
+    }
+    $Menu.Menu.Prompts | foreach {
+        if ($_.ActiveType -eq "TextToSpeech") { $prompt = ("TextToSpeech: " + $_.TextToSpeechPrompt) }
+        elseif ($_ -eq "AudioFile") { $prompt = ("AudioFile: " + $_.AudioFilePrompt) }
+        $item = [PSCustomObject]@{Type='Menu Prompt';Greeting=$prompt}
+        [void]$greetings.Add($item)
+    }
+    $greetings
+}
--- Functions/fn_convertto.ps1
+++ Functions/fn_convertto.ps1
@@ -3,7 +3,10 @@
     [OutputType([string])]
     Param ([Parameter(Mandatory = $true,Position = 0,ValueFromPipeline = $true)][PSObject[]]$InputObject)
     # Code borrowed from https://www.powershellgallery.com/packages/PSMarkdown/1.1
-    # Modified to preserve order of properties
+    # Modified to preserve order of properties and escape special characters
+    # Lines 16,34,41,49 escape the pipe character in the unlikely event a property name contains one
+    # Lines 52-53 escape special characters from property values
+    # More escaping may be needed after additional testing
     Begin {
         $items   = @()
         $columns = @{}
@@ -12,6 +15,7 @@
         foreach ($item in $InputObject) {
             $items += $item
             foreach ($property in $item.PSObject.Properties) {
+                $property.Value = ($property.Value -replace '\|','\|')
                 if ($property.Value -ne $null) {
                     if (-not $columns.ContainsKey($property.Name) -or $columns[$property.Name] -lt $property.Value.ToString().Length) {
                         $columns[$property.Name] = $property.Value.ToString().Length
@@ -29,18 +33,26 @@
         # Add whitespace to header row for consistent column width throughout table
         $header = @()
         foreach ($property in $headerNames) {
+            $property = ($property -replace '\|','\|')
             $header += ('{0,-' + $columns[$property] + '}') -f $property
         }
         ("| " + ($header -join ' | ') + " |")
         # Create delimiter row
         $delimiter = @()
-        foreach ($property in $headerNames) { $delimiter += '-' * $columns[$property] }
+        foreach ($property in $headerNames) {
+            $property = ($property -replace '\|','\|')
+            $delimiter += '-' * $columns[$property]
+        }
         ("| " + ($delimiter -join ' | ') + " |")
         # Add whitespace to header row for consistent column width throughout table
         foreach ($item in $items) {
             $values = @()
             foreach ($property in $headerNames) {
-                $values += ('{0,-' + $columns[$property] + '}') -f $item.($property)
+                $property = ($property -replace '\|','\|')
+                $value = $item.($property)
+                $value = ($value -replace '\|','\|')
+                $value = ($value -replace '\\\+','\\+')
+                $values += ('{0,-' + $columns[$property] + '}') -f $value
             }
             ("| " + ($values -join ' | ') + " |")
         }
