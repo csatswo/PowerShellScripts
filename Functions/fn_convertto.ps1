d1445cb0f30b521c162470f0ee187fa4d8146fb3
Chris <68028099+csatswo@users.noreply.github.com>
2023-07-01 08:44:39 -0700

    Update fn_convertto.ps1

--- Functions/fn_convertto.ps1
+++ Functions/fn_convertto.ps1
@@ -1,31 +1,37 @@
 ﻿Function ConvertTo-MDTable {
-    [CmdletBinding()]
-    [OutputType([string])]
-    Param ([Parameter(Mandatory = $true,Position = 0,ValueFromPipeline = $true)][PSObject[]]$InputObject)
+    [CmdletBinding()][OutputType([string])]Param (
+        [Parameter(Mandatory = $true,Position = 0,ValueFromPipeline = $true)][PSObject[]]$InputObject,
+        [Parameter(mandatory=$false)][switch]$HideHeaders
+    )
     # Code borrowed from https://www.powershellgallery.com/packages/PSMarkdown/1.1
-    # Modified to preserve order of properties and escape special characters
-    # Lines 16,34,41,49 escape the pipe character in the unlikely event a property name contains one
-    # Lines 52-53 escape special characters from property values
+    # Modified to preserve order of properties, escape special characters, and allow an unpopulated header row
     # More escaping may be needed after additional testing
     Begin {
         $items   = @()
         $columns = @{}
+        Function EscChar {
+            [CmdletBinding()][OutputType([string])]Param([Parameter(Mandatory = $true,Position = 0,ValueFromPipeline = $true)]$Chars)
+            $Chars = $Chars -replace '\|','\|'
+            $Chars = $Chars -replace '\+','\+'
+            $Chars
+        }
     }
     Process {
         foreach ($item in $InputObject) {
             $items += $item
             foreach ($property in $item.PSObject.Properties) {
-                $property.Value = ($property.Value -replace '\|','\|')
                 if ($property.Value -ne $null) {
-                    if (-not $columns.ContainsKey($property.Name) -or $columns[$property.Name] -lt $property.Value.ToString().Length) {
-                        $columns[$property.Name] = $property.Value.ToString().Length
+                    $propertyName = $property.Name | EscChar
+                    $propertyValue = $property.Value | EscChar
+                    if (-not $columns.ContainsKey($property.Name) -or $columns[$property.Name] -lt $propertyValue.ToString().Length) {
+                        $columns[$property.Name] = $propertyValue.ToString().Length
                     }
                 }
             }
         }
     }
     End {
-        $headerNames  = @($item.PSObject.Properties.Name)
+        $headerNames = @($item.PSObject.Properties.Name)
         # Get column width sizing
         foreach ($key in $($columns.Keys)) {
             $columns[$key] = [Math]::Max($columns[$key], $key.Length)
@@ -33,26 +39,26 @@
         # Add whitespace to header row for consistent column width throughout table
         $header = @()
         foreach ($property in $headerNames) {
-            $property = ($property -replace '\|','\|')
-            $header += ('{0,-' + $columns[$property] + '}') -f $property
+            $fproperty = $property | EscChar
+            if ($HideHeaders) {$fproperty = ($fproperty -replace '.',' ')}
+            $header += (('{0,-' + $columns[$property] + '}') -f $fproperty)
         }
         ("| " + ($header -join ' | ') + " |")
         # Create delimiter row
         $delimiter = @()
         foreach ($property in $headerNames) {
-            $property = ($property -replace '\|','\|')
+            # $property = $property | EscChar
             $delimiter += '-' * $columns[$property]
         }
         ("| " + ($delimiter -join ' | ') + " |")
         # Add whitespace to header row for consistent column width throughout table
         foreach ($item in $items) {
             $values = @()
-            foreach ($property in $headerNames) {
-                $property = ($property -replace '\|','\|')
+            foreach ($property in $item.PSObject.Properties.Name) {
+                # $property = $property | EscChar
                 $value = $item.($property)
-                $value = ($value -replace '\|','\|')
-                $value = ($value -replace '\\\+','\\+')
-                $values += ('{0,-' + $columns[$property] + '}') -f $value
+                $value = $value | EscChar
+                $values += ('{0,-' + $columns[($property | EscChar)] + '}') -f $value
             }
             ("| " + ($values -join ' | ') + " |")
         }
