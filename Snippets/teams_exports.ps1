6c224ea0925f957382e12521d05675c0a9196359
Chris <68028099+csatswo@users.noreply.github.com>
2023-09-17 23:53:53 -0700

    Update teams_exports.ps1

--- Snippets/teams_exports.ps1
+++ Snippets/teams_exports.ps1
@@ -1,8 +1,14 @@
 $tenant = "demo"
 
-$csOnlineUsers = Get-CsOnlineUser
-$csOnlineUsers | ConvertTo-Json -Depth 100 | Out-File "C:\TEMP\$($tenant)_CsOnlineUsers_2023-09-17.json"
+$timestamp = Get-Date -Format "yyyy-MM-dd-HHmmss"
+$outputFolder = New-Item -Path $PWD\$($tenant)_$($timestamp) -ItemType Directory
 
-foreach ($policyType in (Get-Command get-cs*policy*)) {
-    & $policyType | ConvertTo-Json -Depth 100 | Out-File "C:\TEMP\$($tenant)_$($policyType)_2023-09-17.json"
+$csOnlineUsers = Get-CsOnlineUser
+$csOnlineUsers | ConvertTo-Json -Depth 100 | Out-File "$($outputFolder.FullName)\$($tenant)_CsOnlineUsers.json"
+
+foreach ($policyCmdlet in (Get-Command get-cs*policy).Name) {
+    # Write-Output $policyCmdlet
+    & $policyCmdlet | ConvertTo-Json -Depth 100 | Out-File "$($outputFolder.FullName)\$($tenant)_$($policyCmdlet).json"
 }
+
+Get-CsGroupPolicyAssignment | ConvertTo-Json -Depth 100 | Out-File "$($outputFolder.FullName)\$($tenant)_Get-CsGroupPolicyAssignment.json"
