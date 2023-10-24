c79e3305f701d73b5c88156c30caa447c4b8b6e7
Chris <68028099+csatswo@users.noreply.github.com>
2023-10-24 01:08:23 -0700

    Create combine_teams_net_assess_results.ps1

---
+++ Snippets/combine_teams_net_assess_results.ps1
@@ -0,0 +1,10 @@
+﻿$resultsFolder = "C:\TEMP\TestResults"
+Get-ChildItem -Path $resultsFolder -Directory | foreach -pv dir {$_} | foreach {
+    $results = [System.Collections.ArrayList]@()
+    Get-ChildItem -Path $dir.FullName -Recurse -Include *.csv | foreach -pv csv {$_} | foreach {
+        Import-Csv -Path $csv.FullName | foreach {
+            [void]$results.Add($_)
+        }
+    }
+    $results | Export-Csv -Path $($dir.FullName + "\" + $dir.Name + ".csv") -NoTypeInformation
+}
