7dba6d2170972a8672c3702251153c8f1dfd0926
Chris <68028099+csatswo@users.noreply.github.com>
2023-12-14 13:01:34 -0800

    Update progress_bar.ps1

--- Snippets/progress_bar.ps1
+++ Snippets/progress_bar.ps1
@@ -1,7 +1,12 @@
 # progress_bar.ps1
-# Displays a progress bar while looping through an array
 
+# Displays a progress bar while looping through an array
 $i = 0; foreach ($thing in $array) {
     $i++; $percentComplete = [int](($i / $array.Count) * 100)
     Write-Progress -Activity "Processing thing: $($thing.SpecifcProperty)" -Status "$percentComplete% Complete:" -PercentComplete $percentComplete
+}
+
+# Displays a line if text showing progress while looping through an array
+$linesProcessed = 0; $array | ForEach-Object {
+    ++$linesProcessed; Write-Host -NoNewline $("`rProcessing $($linesProcessed)/$($array.Count): $([int]($linesProcessed / $array.Count * 100))% Complete    ")
 }
