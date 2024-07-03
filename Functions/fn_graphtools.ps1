c1aeed71537efa8925ff18e12565be72a6a8499d
Chris <68028099+csatswo@users.noreply.github.com>
2024-07-02 21:49:58 -0700

    Update fn_graphtools.ps1

--- Functions/fn_graphtools.ps1
+++ Functions/fn_graphtools.ps1
@@ -36,8 +36,8 @@
 
 Function Get-AADAuthToken {
     [CmdletBinding()][OutputType([string])]Param (
-        [Parameter(Mandatory=$true)][ValidateScript({try{ [System.Guid]::Parse($_) | Out-Null;$true } catch { $false }})][string]$TenantId,
-        [Parameter(Mandatory=$true)][ValidateScript({try{ [System.Guid]::Parse($_) | Out-Null;$true } catch { $false }})][string]$ClientId,
+        [Parameter(Mandatory=$true)][Guid]$TenantId,
+        [Parameter(Mandatory=$true)][Guid]$ClientId,
         [Parameter(Mandatory=$true)][string[]]$Scopes,
         [Parameter(Mandatory=$false)][string]$Username
     )
@@ -67,4 +67,52 @@ Function Get-AADAuthToken {
     }
     $tokenRequest = Invoke-RestMethod -Method Post -Uri $uri -ContentType "application/x-www-form-urlencoded" -Body $body -UseBasicParsing
     $tokenRequest
+}
+
+Function Get-AADAuthTokenFromCert {
+    [CmdletBinding()][OutputType([string])]Param(
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true)][string]$Thumbprint,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true)][guid]$ClientId,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true)][guid]$TenantId
+    )
+    $cert       = @(Get-ChildItem -Path Cert:\CurrentUser\My\$thumbprint)[0]
+    $certHash   = [System.Convert]::ToBase64String($cert.GetCertHash())
+    $scope      = "https://graph.microsoft.com/.default"
+    $tokenUrl   = "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token"
+    $jwtHeader = @{
+        alg = "RS256"
+        typ = "JWT"
+        x5t = $certHash -replace '\+','-' -replace '/','_' -replace '='
+    }
+    $jwtPayLoad = @{
+        aud = $tokenUrl
+        exp = [Math]::Round((New-TimeSpan -Start ([DateTime]'1970-01-01Z') -End (Get-Date).ToUniversalTime().AddMinutes(2)).TotalSeconds,0)
+        iss = $clientId
+        jti = [guid]::NewGuid()
+        sub = $clientId
+    }
+    $encodedHeader  = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes(($jwtHeader | ConvertTo-Json)))
+    $encodedPayload = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes(($jwtPayLoad | ConvertTo-Json)))
+    $privateKey     = ([System.Security.Cryptography.X509Certificates.RSACertificateExtensions]::GetRSAPrivateKey($cert))
+    $rsaPadding     = [Security.Cryptography.RSASignaturePadding]::Pkcs1
+    $hashAlgo       = [Security.Cryptography.HashAlgorithmName]::SHA256
+    $signature      = [Convert]::ToBase64String($privateKey.SignData([System.Text.Encoding]::UTF8.GetBytes($encodedHeader+"."+$encodedPayload),$hashAlgo,$rsaPadding)) -replace '\+','-' -replace '/','_' -replace '='
+    $jwt            = $encodedHeader,$encodedPayload,$signature -join "."
+    $header         = @{ Authorization = "Bearer $jwt"}
+    $body = @{
+        client_id             = $clientId
+        client_assertion      = $jwt
+        client_assertion_type = "urn:ietf:params:oauth:client-assertion-type:jwt-bearer"
+        scope                 = $scope
+        grant_type            = "client_credentials"
+    }
+    $params = @{
+        ContentType = 'application/x-www-form-urlencoded'
+        Method       = 'POST'
+        Body         = $body
+        Uri          = $tokenUrl
+        Headers      = $header
+    }
+    $tokenRequest = Invoke-RestMethod @params
+    $tokenRequest.access_token
 }
