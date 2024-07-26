d7869f02490194cbb122913df25992f74f20ed43
Chris <68028099+csatswo@users.noreply.github.com>
2024-07-26 15:18:55 -0700

    Update fn_formatting.ps1

--- Functions/fn_formatting.ps1
+++ Functions/fn_formatting.ps1
@@ -1,4 +1,9 @@
-﻿New-Alias -Name fm -Value Format-Markdown -Description 'fm -> Format-Markdown'
+﻿Function Set-ISETitle {
+    Param ([Parameter(Mandatory=$true,Position=0,ValueFromPipeline=$true)][String]$Title)
+    $Host.UI.RawUI.WindowTitle = $Title
+}
+
+New-Alias -Name fm -Value Format-Markdown -Description 'fm -> Format-Markdown'
 Function Format-Markdown {
     [CmdletBinding()][OutputType([string])]Param (
         [Parameter(Mandatory = $true,Position = 0,ValueFromPipeline = $true)][PSObject[]]$InputObject,
@@ -18,52 +23,56 @@ Function Format-Markdown {
         }
     }
     Process {
-        foreach ($item in $InputObject) {
-            $items += $item
-            foreach ($property in $item.PSObject.Properties) {
-                if ($null -ne $property.Value) {
-                    $propertyName = $property.Name | EscChar
-                    $propertyValue = $property.Value | EscChar
-                }
-                if (-not $columns.ContainsKey($property.Name) -or $columns[$property.Name] -lt $propertyValue.ToString().Length) {
-                    $columns[$property.Name] = $propertyValue.ToString().Length
+        if ($InputObject) {
+            foreach ($item in $InputObject) {
+                $items += $item
+                foreach ($property in $item.PSObject.Properties) {
+                    if ($null -ne $property.Value) {
+                        $propertyName = $property.Name | EscChar
+                        $propertyValue = $property.Value | EscChar
+                    }
+                    if (-not $columns.ContainsKey($property.Name) -or $columns[$property.Name] -lt $propertyValue.ToString().Length) {
+                        $columns[$property.Name] = $propertyValue.ToString().Length
+                    }
                 }
             }
         }
     }
     End {
-        $headerNames = @($item.PSObject.Properties.Name)
-        # Get column width sizing
-        foreach ($key in $($columns.Keys)) {
-            $columns[$key] = [Math]::Max($columns[$key], $key.Length)
-        }
-        # Add whitespace to header row for consistent column width throughout table
-        $header = @()
-        foreach ($property in $headerNames) {
-            $fproperty = $property | EscChar
-            if ($HideHeaders) {$fproperty = ($fproperty -replace '.',' ')}
-            $header += (('{0,-' + $columns[$property] + '}') -f $fproperty)
-        }
-        ("| " + ($header -join ' | ') + " |")
-        # Create delimiter row
-        $delimiter = @()
-        foreach ($property in $headerNames) {
-            # $property = $property | EscChar
-            $delimiter += '-' * $columns[$property]
-        }
-        ("| " + ($delimiter -join ' | ') + " |")
-        # Add whitespace to header row for consistent column width throughout table
-        foreach ($item in $items) {
-            $values = @()
-            foreach ($property in $item.PSObject.Properties.Name) {
-                # $property = $property | EscChar
-                $value = $item.($property)
-                if ($null -ne $value ) {
-                    $value = $value | EscChar
-                }
-                $values += ('{0,-' + $columns[($property | EscChar)] + '}') -f $value
+        if ($InputObject) {
+            $headerNames = @($item.PSObject.Properties.Name)
+            # Get column width sizing
+            foreach ($key in $($columns.Keys)) {
+                $columns[$key] = [Math]::Max($columns[$key], $key.Length)
             }
-            ("| " + ($values -join ' | ') + " |")
+            # Add whitespace to header row for consistent column width throughout table
+            $header = @()
+            foreach ($property in $headerNames) {
+                $fproperty = $property | EscChar
+                if ($HideHeaders) {$fproperty = ($fproperty -replace '.',' ')}
+                $header += (('{0,-' + $columns[$property] + '}') -f $fproperty)
+            }
+            ("| " + ($header -join ' | ') + " |")
+            # Create delimiter row
+            $delimiter = @()
+            foreach ($property in $headerNames) {
+                # $property = $property | EscChar
+                $delimiter += '-' * $columns[$property]
+            }
+            ("| " + ($delimiter -join ' | ') + " |")
+            # Add whitespace to header row for consistent column width throughout table
+            foreach ($item in $items) {
+                $values = @()
+                foreach ($property in $item.PSObject.Properties.Name) {
+                    # $property = $property | EscChar
+                    $value = $item.($property)
+                    if ($null -ne $value ) {
+                        $value = $value | EscChar
+                    }
+                    $values += ('{0,-' + $columns[($property | EscChar)] + '}') -f $value
+                }
+                ("| " + ($values -join ' | ') + " |")
+            }
         }
     }
 }
